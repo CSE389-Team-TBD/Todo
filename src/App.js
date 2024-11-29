57ebd68dcbf8119a56dc4a422f109c1b03e213ac
Adya Parida <82367239+adyaparida@users.noreply.github.com>
2024-11-29 17:56:12 -0500

    Updated App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth";
+import ToDoList from "./components/ToDoList";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/todos" element={<ToDoList />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
