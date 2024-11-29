e62482c2081f5eacacd9103d56a297bce9eba325
Adya Parida <82367239+adyaparida@users.noreply.github.com>
2024-11-29 18:01:35 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./components/Auth";
 import ToDoList from "./components/ToDoList";
+import './App.css';
+
 
 function App() {
   return (
